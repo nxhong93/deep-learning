bed0f88bdfcc2d39f28535e8339ab25c11de040e
nxhong93 <34727338+nxhong93@users.noreply.github.com>
2018-04-22 22:17:51 +0700

    Add files via upload

---
+++ stock/cp.py
@@ -0,0 +1,201 @@
+import numpy as np
+import pandas as pd
+import math
+import sklearn
+import sklearn.preprocessing
+from datetime import datetime
+import matplotlib.dates as dt
+import matplotlib.pyplot as plt
+from matplotlib.finance import candlestick_ohlc, volume_overlay
+import matplotlib.ticker as ticker
+import matplotlib
+import tensorflow as tf
+import matplotlib.dates as mdates
+
+valid_set_size_percentage = 10
+test_set_size_percentage = 10
+
+df = pd.read_csv('CafeF.HSX.Upto03.04.2018.csv')
+print(df)
+ticket = df['<Ticker>']
+print('number of different stocks: ', len(list(set(ticket))))
+df.tail()
+
+def chart(symbol):
+	data = df[df['<Ticker>'] == symbol][['<DTYYYYMMDD>','<Open>','<High>','<Low>','<Close>','<Volume>']]
+	fig = plt.figure()
+	ax = plt.subplot2grid((4,4), (0,0), rowspan=3, colspan=4)
+	data['<DTYYYYMMDD>'] = data['<DTYYYYMMDD>'].apply(lambda x: datetime.strptime(str(x), '%Y%m%d'))
+	data['<DTYYYYMMDD>'] = data['<DTYYYYMMDD>'].map(dt.date2num)
+
+	candlestick_ohlc(ax, data.values, width=0.5, colorup='#53c156', colordown='#ff1717')
+	ax.set_title(symbol)
+	ax.set_ylabel('Price')
+	ax.set_xlabel('Date')
+	ax.axes.get_xaxis().set_visible(False)
+
+	ax2 = plt.subplot2grid((4,4), (3,0), rowspan=1, colspan=4)
+	bc = volume_overlay(ax2, data['<Open>'][::-1], data['<Close>'][::-1], data['<Volume>'][::-1], colorup='g', alpha=0.5, width=1)
+	ax2.add_collection(bc)
+	# ax2.xaxis_date()
+	# ax2.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+	# fig.autofmt_xdate()
+	# ax2.grid(True)
+	plt.show()
+	return data
+cp = chart('VCB')
+def normalize_data(df):
+    min_max_scaler = sklearn.preprocessing.MinMaxScaler()
+    df['<Open>'] = min_max_scaler.fit_transform(df['<Open>'].values.reshape(-1,1))
+    df['<High>'] = min_max_scaler.fit_transform(df['<High>'].values.reshape(-1,1))
+    df['<Low>'] = min_max_scaler.fit_transform(df['<Low>'].values.reshape(-1,1))
+    df['<Close>'] = min_max_scaler.fit_transform(df['<Close>'].values.reshape(-1,1))
+    df = df.iloc[:,1:]
+    return df
+
+
+
+def load_data(stock, seq_len):
+    data_raw = stock.as_matrix() # convert to numpy array
+    data = []
+    
+    # create all possible sequences of length seq_len
+    for index in range(len(data_raw) - seq_len): 
+        data.append(data_raw[index: index + seq_len])
+    data = np.array(data)
+    valid_set_size = int(np.round(valid_set_size_percentage/100*data.shape[0]))
+    test_set_size = int(np.round(test_set_size_percentage/100*data.shape[0]))
+    train_set_size = data.shape[0] - (valid_set_size + test_set_size)
+    
+    x_train = data[:train_set_size,:-1,:]
+    y_train = data[:train_set_size,-1,:]
+    
+    x_valid = data[train_set_size:train_set_size+valid_set_size,:-1,:]
+    y_valid = data[train_set_size:train_set_size+valid_set_size,-1,:]
+    
+    x_test = data[train_set_size+valid_set_size:,:-1,:]
+    y_test = data[train_set_size+valid_set_size:,-1,:]
+    
+    return [x_train, y_train, x_valid, y_valid, x_test, y_test]
+
+df_stock = cp.copy()
+df_stock.drop(['<Volume>'],1,inplace=True)
+# print(df_stock)
+
+
+cols = list(df_stock.columns.values)
+print('df_stock.columns.values = ', cols)
+
+# normalize stock
+df_stock_norm = df_stock.copy()
+df_stock_norm = normalize_data(df_stock_norm)
+
+# create train, test data
+seq_len = 20
+x_train, y_train, x_valid, y_valid, x_test, y_test = load_data(df_stock_norm, seq_len)
+
+index_in_epoch = 0
+perm_array  = np.arange(x_train.shape[0])
+np.random.shuffle(perm_array)
+
+
+def get_next_batch(batch_size):
+    global index_in_epoch, x_train, perm_array   
+    start = index_in_epoch
+    index_in_epoch += batch_size
+    
+    if index_in_epoch > x_train.shape[0]:
+        np.random.shuffle(perm_array)
+        start = 0
+        index_in_epoch = batch_size
+        
+    end = index_in_epoch
+    return x_train[perm_array[start:end]], y_train[perm_array[start:end]]
+
+
+n_steps = seq_len-1 
+n_inputs = 4 
+n_neurons = 200 
+n_outputs = 4
+n_layers = 2
+learning_rate = 0.01
+batch_size = 50
+n_epochs = 100 
+train_set_size = x_train.shape[0]
+test_set_size = x_test.shape[0]
+
+tf.reset_default_graph()
+
+X = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+y = tf.placeholder(tf.float32, [None, n_outputs])
+
+
+layers = [tf.contrib.rnn.BasicRNNCell(num_units=n_neurons, activation=tf.nn.elu) for layer in range(n_layers)]
+
+# use Basic LSTM Cell 
+#layers = [tf.contrib.rnn.BasicLSTMCell(num_units=n_neurons, activation=tf.nn.elu) for layer in range(n_layers)]
+
+# use LSTM Cell with peephole connections
+#layers = [tf.contrib.rnn.LSTMCell(num_units=n_neurons, activation=tf.nn.leaky_relu, use_peepholes = True) for layer in range(n_layers)]
+
+# use GRU cell
+#layers = [tf.contrib.rnn.GRUCell(num_units=n_neurons, activation=tf.nn.leaky_relu) for layer in range(n_layers)]
+                                                                     
+multi_layer_cell = tf.contrib.rnn.MultiRNNCell(layers)
+rnn_outputs, states = tf.nn.dynamic_rnn(multi_layer_cell, X, dtype=tf.float32)
+
+stacked_rnn_outputs = tf.reshape(rnn_outputs, [-1, n_neurons]) 
+stacked_outputs = tf.layers.dense(stacked_rnn_outputs, n_outputs)
+outputs = tf.reshape(stacked_outputs, [-1, n_steps, n_outputs])
+outputs = outputs[:,n_steps-1,:]
+                                              
+loss = tf.reduce_mean(tf.square(outputs - y)) 
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(loss)
+                                              
+
+with tf.Session() as sess: 
+    sess.run(tf.global_variables_initializer())
+    for iteration in range(int(n_epochs*train_set_size/batch_size)):
+        x_batch, y_batch = get_next_batch(batch_size)
+        sess.run(training_op, feed_dict={X: x_batch, y: y_batch}) 
+        if iteration % int(5*train_set_size/batch_size) == 0:
+            mse_train = loss.eval(feed_dict={X: x_train, y: y_train}) 
+            mse_valid = loss.eval(feed_dict={X: x_valid, y: y_valid}) 
+            print('%.2f epochs: MSE train/valid = %.6f/%.6f'%(
+                iteration*batch_size/train_set_size, mse_train, mse_valid))
+
+    y_train_pred = sess.run(outputs, feed_dict={X: x_train})
+    y_valid_pred = sess.run(outputs, feed_dict={X: x_valid})
+    y_test_pred = sess.run(outputs, feed_dict={X: x_test})
+
+ft = 0 # 0 = open, 1 = close, 2 = highest, 3 = lowest
+
+
+plt.figure(figsize=(15, 5))
+plt.subplot(1,2,1)
+
+plt.plot(np.arange(y_train.shape[0]), y_train[:,ft], color='blue', label='train target')
+plt.plot(np.arange(y_train.shape[0], y_train.shape[0]+y_valid.shape[0]), y_valid[:,ft], color='gray', label='valid target')
+plt.plot(np.arange(y_train.shape[0]+y_valid.shape[0], y_train.shape[0]+y_test.shape[0]+y_test.shape[0]), y_test[:,ft], color='black', label='test target')
+plt.plot(np.arange(y_train_pred.shape[0]),y_train_pred[:,ft], color='red', label='train prediction')
+plt.plot(np.arange(y_train_pred.shape[0], y_train_pred.shape[0]+y_valid_pred.shape[0]), y_valid_pred[:,ft], color='orange', label='valid prediction')
+plt.plot(np.arange(y_train_pred.shape[0]+y_valid_pred.shape[0], y_train_pred.shape[0]+y_valid_pred.shape[0]+y_test_pred.shape[0]), y_test_pred[:,ft], color='green', label='test prediction')
+plt.title('past and future stock prices')
+plt.xlabel('time [days]')
+plt.ylabel('normalized price')
+plt.legend(loc='best')
+
+plt.subplot(1,2,2)
+plt.plot(np.arange(y_train.shape[0], y_train.shape[0]+y_test.shape[0]), y_test[:,ft], color='black', label='test target')
+plt.plot(np.arange(y_train_pred.shape[0], y_train_pred.shape[0]+y_test_pred.shape[0]), y_test_pred[:,ft], color='green', label='test prediction')
+plt.title('future stock prices')
+plt.xlabel('time [days]')
+plt.ylabel('normalized price')
+plt.legend(loc='best')
+plt.show()
+corr_price_development_train = np.sum(np.equal(np.sign(y_train[:,1]-y_train[:,0]), np.sign(y_train_pred[:,1]-y_train_pred[:,0])).astype(int)) / y_train.shape[0]
+corr_price_development_valid = np.sum(np.equal(np.sign(y_valid[:,1]-y_valid[:,0]), np.sign(y_valid_pred[:,1]-y_valid_pred[:,0])).astype(int)) / y_valid.shape[0]
+corr_price_development_test = np.sum(np.equal(np.sign(y_test[:,1]-y_test[:,0]), np.sign(y_test_pred[:,1]-y_test_pred[:,0])).astype(int)) / y_test.shape[0]
+
+print('correct sign prediction for close - open price for train/valid/test: %.2f/%.2f/%.2f'%(corr_price_development_train, corr_price_development_valid, corr_price_development_test))
